dad2984ead8ce656e48fe6dcd5764bd47fbb3558
mamesareagan <mamesareagan0@gmail.com>
2023-08-22 21:57:03 +0300

    fixed betty checking style

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,49 +2,42 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Deletes a node at a specific index from a list.
- * @head: A pointer to the first element of a list.
- * @index: The index of the node to delete.
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current;
-    unsigned int i;
+dlistint_t *current;
+unsigned int i;
 
-    /* Check if the list is empty */
-    if (*head == NULL)
-        return (-1);
+if (*head == NULL)
+return (-1);
 
-    current = *head;
+current = *head;
 
-    /* Special case: Delete the head node */
-    if (index == 0)
-    {
-        *head = current->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(current);
-        return (1);
-    }
-
-    /* Traverse to the node to delete */
-    for (i = 0; current != NULL && i < index; i++)
-        current = current->next;
-
-    /* If the node was not found at the given index */
-    if (current == NULL)
-        return (-1);
-
-    /* Update the previous and next pointers of neighboring nodes */
-    if (current->prev != NULL)
-        current->prev->next = current->next;
-    if (current->next != NULL)
-        current->next->prev = current->prev;
-
-    /* Delete the current node */
-    free(current);
-    return (1);
+if (index == 0)
+{
+*head = current->next;
+if (*head != NULL)
+(*head)->prev = NULL;
+free(current);
+return (1);
 }
 
+for (i = 0; current != NULL && i < index; i++)
+current = current->next;
+
+if (current == NULL)
+return (-1);
+
+if (current->prev != NULL)
+current->prev->next = current->next;
+if (current->next != NULL)
+current->next->prev = current->prev;
+
+free(current);
+return (1);
+}
